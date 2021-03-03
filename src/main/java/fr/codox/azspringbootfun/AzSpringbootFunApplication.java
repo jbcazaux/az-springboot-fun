e5424c787cb4134085e4d648e36c7ad06aedf9e7
jbcazaux <jean-baptiste.cazaux@externe.maif.fr>
2021-03-03 18:27:53 +0100

    hello world

---
+++ src/main/java/fr/codox/azspringbootfun/AzSpringbootFunApplication.java
@@ -0,0 +1,13 @@
+package fr.codox.azspringbootfun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzSpringbootFunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzSpringbootFunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/codox/azspringbootfun/AzSpringbootFunApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.codox.azspringbootfun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzSpringbootFunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
