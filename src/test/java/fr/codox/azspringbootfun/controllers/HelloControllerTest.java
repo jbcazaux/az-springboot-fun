b652a7cb2ca29c7831ff546ae80bfdf84c95b7fe
jbcazaux <jean-baptiste.cazaux@externe.maif.fr>
2021-03-12 12:51:55 +0100

    use custom dockerfile

--- src/test/java/fr/codox/azspringbootfun/controllers/HelloControllerTest.java
+++ src/test/java/fr/codox/azspringbootfun/controllers/HelloControllerTest.java
@@ -1,28 +1,41 @@
 package fr.codox.azspringbootfun.controllers;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.MediaType.TEXT_PLAIN;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+import fr.codox.azspringbootfun.services.HelloService;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 @AutoConfigureMockMvc
 class HelloControllerTest {
+    @LocalServerPort
+    private int port;
+
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
+
+    @MockBean
+    private HelloService helloService;
 
     @Test
     public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(TEXT_PLAIN))
+        Mockito.when(helloService.hello("world")).thenReturn("foo");
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/").accept(TEXT_PLAIN))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello world !")));
+                .andExpect(content().string(equalTo("foo")));
     }
 }
