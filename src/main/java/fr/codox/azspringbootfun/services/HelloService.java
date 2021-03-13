74d0b89a326f094ad6184a3f90d58b8dd72c45ab
jbcazaux <jean-baptiste.cazaux@externe.maif.fr>
2021-03-13 17:44:40 +0100

    update hello service

--- src/main/java/fr/codox/azspringbootfun/services/HelloService.java
+++ src/main/java/fr/codox/azspringbootfun/services/HelloService.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Service;
 public class HelloService {
 
     public String hello(String name) {
-        return "Hello " + name + " !";
+        return "Hello " + name + " !!";
     }
 }
