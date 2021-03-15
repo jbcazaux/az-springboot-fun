ece0d1fdcc8870a0c09e520430e3b0a17c6f6dd4
jbcazaux <jean-baptiste.cazaux@externe.maif.fr>
2021-03-15 20:07:00 +0100

    properties from Azure app configuration

--- src/main/java/fr/codox/azspringbootfun/AzSpringbootFunApplication.java
+++ src/main/java/fr/codox/azspringbootfun/AzSpringbootFunApplication.java
@@ -2,8 +2,12 @@ package fr.codox.azspringbootfun;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import fr.codox.azspringbootfun.configurations.MessageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MessageProperties.class)
 public class AzSpringbootFunApplication {
 
     public static void main(String[] args) {
--- src/main/java/fr/codox/azspringbootfun/controllers/HelloController.java
+++ src/main/java/fr/codox/azspringbootfun/controllers/HelloController.java
@@ -3,19 +3,31 @@ package fr.codox.azspringbootfun.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.codox.azspringbootfun.configurations.MessageProperties;
 import fr.codox.azspringbootfun.services.HelloService;
 
 @RestController
 public class HelloController {
 
     private final HelloService helloService;
+    private final MessageProperties messageProperties;
 
-    public HelloController(HelloService helloService) {
+    public HelloController(HelloService helloService, MessageProperties messageProperties) {
         this.helloService = helloService;
+        this.messageProperties = messageProperties;
     }
 
     @GetMapping
     public String hello() {
         return helloService.hello("world");
+    }
+
+    @GetMapping("/message")
+    public String message() {
+        return messageProperties.getMessage();
+    }
+    @GetMapping("toto")
+    public String message2() {
+        return messageProperties.getMessage();
     }
 }
